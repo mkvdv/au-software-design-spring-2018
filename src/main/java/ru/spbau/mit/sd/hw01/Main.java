2135b9af9173668f5ab788c4b09414047dadec02
avallon <makeevvolodja@gmail.com>
2018-02-25 17:02:04 +0300

    restore proper main function

--- src/main/java/ru/spbau/mit/sd/hw01/Main.java
+++ src/main/java/ru/spbau/mit/sd/hw01/Main.java
@@ -1,23 +1,8 @@
 package ru.spbau.mit.sd.hw01;
 
-import java.util.Arrays;
-
 public class Main {
-
-    public static void main(String[] args_) {
-//        Shell sh = new Shell();
-//        sh.run();
-
-        String s = "\"cat$dog in cat\\$dog kitchen near \\\"$dog\\\"";
-        String regex = "(?=\\\\\\$|\\s|\\$|\\\\\'|\\\\\")";
-        System.out.println(Arrays.toString(s.split(regex)));
-
-        StringBuilder sb = new StringBuilder();
-        for (String str : s.split(regex)) {
-            sb.append(str);
-        }
-        System.out.println(s);
-        System.out.println(sb.toString());
-
+    public static void main(String[] args) {
+        Shell sh = new Shell();
+        sh.run();
     }
 }
