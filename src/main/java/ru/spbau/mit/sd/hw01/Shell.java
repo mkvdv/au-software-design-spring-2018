75bb5eaf337cce01ad8e608daaafdfadc15bea7b
avallon <makeevvolodja@gmail.com>
2018-03-02 14:35:09 +0300

    fixed ctrl-d explosion

--- src/main/java/ru/spbau/mit/sd/hw01/Shell.java
+++ src/main/java/ru/spbau/mit/sd/hw01/Shell.java
@@ -51,6 +51,8 @@ public class Shell {
                         e.printStackTrace();
                     }
                 }
+            } else {
+                return;
             }
         }
     }
